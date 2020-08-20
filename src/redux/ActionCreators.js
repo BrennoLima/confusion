2afe7c56330da54b11a5eb9fb0fe5b035f7074d3
= <brennocesar@live.com>
2020-08-20 11:34:30 -0400

    React Thunk

--- src/redux/ActionCreators.js
+++ src/redux/ActionCreators.js
@@ -1,5 +1,7 @@
 import * as ActionTypes from './ActionTypes';
+import { DISHES } from '../shared/dishes';
 
+// Action
 export const addComment = (dishId, rating, author, comment) => ({
     type: ActionTypes.ADD_COMMENT,
     payload : {
@@ -9,3 +11,28 @@ export const addComment = (dishId, rating, author, comment) => ({
         comment: comment
     }
 });
+
+// THUNK
+export const fetchDishes = () => (dispatch) => {
+    dispatch(dishesLoading(true));
+    setTimeout(() => {
+        dispatch(addDishes(DISHES));
+    }, 2000);
+}
+// Action
+export const dishesLoading = () => ({
+    type: ActionTypes.DISHES_LOADING
+})
+
+//Action
+export const dishesFailed = (errMess) => ({
+    type: ActionTypes.DISHES_FAILED,
+    payload: errMess
+});
+
+// Action
+export const addDishes = (dishes) => ({
+    type: ActionTypes.ADD_DISHES,
+    payload: dishes
+    
+})
