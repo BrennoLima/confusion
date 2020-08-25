294a1768a1e8ca5ad4834a2c7156063fb21904d5
= <brennocesar@live.com>
2020-08-25 10:18:50 -0400

    fetch handling errors

--- src/redux/ActionCreators.js
+++ src/redux/ActionCreators.js
@@ -20,8 +20,26 @@ export const addComments = (comments) => ({
 
 export const fetchComments = () => (dispatch) => {
     return fetch(baseUrl + 'comments')
+            //error handling
+        .then(response => {
+            if(response.ok){
+                return response;
+            }
+            // this first error handler is used when we RECEIVE a error response from server
+            else{
+                var error = new Error('Error ' + response.status +': ' + response.statusText);
+                error.response = response;
+                throw error;
+            }
+        },
+        // this second error handles when we DONT RECEIVE a response from the server
+        error => {
+            var errmess = new Error(error.message);
+            throw errmess;
+        })
         .then(response => response.json())
-        .then(comments => dispatch(addComments(comments)));
+        .then(comments => dispatch(addComments(comments)))
+        .catch(error => dispatch(commentsFailed(error.message)))
 }
 
 export const commentsFailed = (errMess) => ({
@@ -43,8 +61,25 @@ export const dishesLoading = () => ({
 export const fetchDishes = () => (dispatch) => {
     dispatch(dishesLoading(true));
     return fetch(baseUrl + 'dishes')
+        .then(response => {
+            if(response.ok){
+                return response;
+            }
+            // this first error handler is used when we RECEIVE a error response from server
+            else{
+                var error = new Error('Error ' + response.status +': ' + response.statusText);
+                error.response = response;
+                throw error;
+            }
+        },
+        // this second error handles when we DONT RECEIVE a response from the server
+        error => {
+            var errmess = new Error(error.message);
+            throw errmess;
+        })
         .then(response => response.json())
-        .then(dishes => dispatch(addDishes(dishes)));
+        .then(dishes => dispatch(addDishes(dishes)))
+        .catch(error => dispatch(dishesFailed(error.message)))
 }
 
 export const dishesFailed = (errMess) => ({
@@ -67,8 +102,25 @@ export const promosLoading = () => ({
 export const fetchPromos = () => (dispatch) => {
     dispatch(promosLoading(true));
     return fetch(baseUrl + 'promotions')
+        .then(response => {
+            if(response.ok){
+                return response;
+            }
+            // this first error handler is used when we RECEIVE a error response from server
+            else{
+                var error = new Error('Error ' + response.status +': ' + response.statusText);
+                error.response = response;
+                throw error;
+            }
+        },
+        // this second error handles when we DONT RECEIVE a response from the server
+        error => {
+            var errmess = new Error(error.message);
+            throw errmess;
+        })
         .then(response => response.json())
-        .then(promos => dispatch(addPromos(promos)));
+        .then(promos => dispatch(addPromos(promos)))
+        .catch(error => dispatch(promosFailed(error.message)))
 }
 
 export const promosFailed = (errMess) => ({
